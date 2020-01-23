5e3fc977dbca83a19bd1bee85a8412f6709e320e
Scott-Huston <scottphuston@gmail.com>
2020-01-23 10:26:17 -0800

    Finished eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,23 +2,6 @@
 
 import sys
 
-test {1:1,
-      2:2,
-      3:4,
-      4:7,
-      5:13,
-}
-
-4
-
-31
-13
-22
-211
-121
-112
-111
-
 ## 1. find biggest solution that works
 ## 2. rotate solution to the right until you get back to original solution
 ## 3. find next biggest solution, repeat
@@ -26,8 +9,30 @@ test {1:1,
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
 def eating_cookies(n, cache=None):
-  pass
+
+  # if cache is null, initialize it
+  if cache is None or type(cache) == list:
+    cache = {0:1, 1:1, 2:2, 3:4}
+
+  # if answer is in the cache, return answer
+  if n in cache.keys():
+    return cache[n]
+  
+  # initialize ways
+  ways = 0
+
+  # add eating_cookies(n-i) for i in range 1 to 3 as long
+  # as n-i is greater than 0
+  for i in range(3):
+    if (i+1)>0:
+      ways+=eating_cookies(n-(i+1), cache)
+
+  # caching answer
+  cache[n] = ways
+
+  return ways
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
